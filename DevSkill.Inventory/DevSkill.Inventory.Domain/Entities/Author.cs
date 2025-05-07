37fff342289b527b390d5d0eb5ddbabd175adad7
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-05-07 18:13:57 +0600

    Added author, book entity and IEntity interface

---
+++ DevSkill.Inventory/DevSkill.Inventory.Domain/Entities/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class Author : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public double Rating { get; set; }
+    }
+}
---
+++ DevSkill.Inventory/DevSkill.Inventory.Domain/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class Book : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Guid AuthorId { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
