cb854b7a300f6d21e4bef90c51b022cd60bd12df
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-05-08 07:21:40 +0600

    added ApplicationDbContext class

---
+++ DevSkill.Inventory/DevSkill.Inventory.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DevSkill.Inventory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(_connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
