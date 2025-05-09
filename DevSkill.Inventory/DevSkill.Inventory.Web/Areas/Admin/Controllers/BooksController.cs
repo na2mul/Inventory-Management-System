c08ebe80801762dc1b4fc436f0e6dcf6a10fba13
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-05-09 11:01:25 +0600

    Added BooksController with Add method

---
+++ DevSkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using DevSkill.Inventory.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using DevSkill.Inventory.Domain.Services;
+using DevSkill.Inventory.Domain.Entities;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookService _bookService;
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Add()
+        {
+            var model = new AddBookModel();
+            return View(model);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Add(AddBookModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookService.AddBook(new Book { Title = model.Title });
+            }
+            return View(model);
+        }
+
+    }
+}
