be3af8bcd124c48e8ef07f104fab59b7a767c9ea
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-05-21 12:41:59 +0600

    Added MiniORM class and IEntity interface

---
+++ MiniORM/MyORM.cs
@@ -0,0 +1,14 @@
+﻿using MiniORM.Assignment2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class MyORM<G,T> where T : class, IEntity<Guid>, new()
+    {
+
+    }
+}
