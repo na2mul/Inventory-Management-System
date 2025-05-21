776f708a10e8b63c644bc0586a2ed1ece2ebabbc
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-05-21 12:56:22 +0600

    Added Database utility classes

---
+++ MiniORM/DbUtility/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniORM.TestClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.DbUtility
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbConnectionString.connection);
+
+        }
+
+        public DbSet<Instructor> Instructor { get; set; }
+        public DbSet<PresentAddress> PresentAddress { get; set; }
+        public DbSet<PermanentAddress> PermanentAddress { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Phone> Phone { get; set; }
+        public DbSet<Session> Session { get; set; }
+        public DbSet<Topic> Topic { get; set; }
+        public DbSet<AdmissionTest> AdmissionTest { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ MiniORM/DbUtility/DbConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.DbUtility
+{
+    public class DbConnectionString
+    {
+        public static string connection = "Server=.\\SQLEXPRESS;Database=AspnetB11;User Id=aspnetb11;Password=123456;TrustServerCertificate=True;";
+    }
+}
