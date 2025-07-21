77263d1101ebe3c332c46f6aa00f7c01b794fa3e
Nazmul Hasan <hasanmdnazmul2016@gmail.com>
2025-07-21 12:05:29 +0600

    chore(worker): add Worker Service project with Serilog and Autofac configuration
    - Added new Worker project to solution
    - Integrated Serilog for logging and Autofac for DI

---
+++ DevSkill.Inventory/DevSkill.Inventory.Worker/WorkerModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace DevSkill.Inventory.Worker
+{
+    public class WorkerModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+
+        public WorkerModule(string connectionString, string migrationAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
